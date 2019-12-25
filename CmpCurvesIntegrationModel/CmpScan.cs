80cbdfabacef18075b0f768f6bd12f6354f96f48
teodorro <edem.teo@gmail.com>
2019-12-25 18:34:24 +0300

    FileOpener

--- CmpCurvesIntegrationModel/CmpScan.cs
+++ CmpCurvesIntegrationModel/CmpScan.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CmpCurvesIntegrationModel
 {
@@ -8,18 +9,23 @@ namespace CmpCurvesIntegrationModel
     public interface ICmpScan
     {
         List<int[]> Data { get; }
-        int Length { get; }
         double StepX { get; set; }
         double StepTime { get; set; }
-
+        int LengthDimensionless { get; }
+        double Length { get; }
+        int AscanLengthDimensionless { get; }
+        double AscanLength { get; }
     }
 
 
     public class CmpScan : ICmpScan
     {
-        public List<int[]> Data { get; }
-        public int Length => Data.Count;
-        public double StepX { get; set; }
-        public double StepTime { get; set; }
+        public List<int[]> Data { get; } = new List<int[]>();
+        public double StepX { get; set; } = 0.1;
+        public double StepTime { get; set; } = 1;
+        public int LengthDimensionless => Data.Count;
+        public double Length => Data.Count * StepX;
+        public int AscanLengthDimensionless => Data.Any() ? Data.Select(x => x.Length).Min() : -1;
+        public double AscanLength => AscanLengthDimensionless * StepTime;
     }
 }
--- CmpCurvesIntegrationModel/FileOpener.cs
+++ CmpCurvesIntegrationModel/FileOpener.cs
@@ -1,15 +1,74 @@
-﻿namespace CmpCurvesIntegrationModel
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CmpCurvesIntegrationModel
 {
 
     public interface IFileOpener
     {
-        ICmpScan OpenKrotTxt(string file);
+        ICmpScan OpenKrotTxt(string filepath);
+        ICmpScan OpenGeo(string filepath);
     }
 
 
     public class FileOpener : IFileOpener   
     {
-        public ICmpScan OpenKrotTxt(string file)
+        public ICmpScan OpenKrotTxt(string filepath)
+        {
+            var lines = File.ReadAllLines(filepath);
+            if (lines.Length < 2)
+                throw new FileLoadException("Ошибка при загрузке файла. Количество строк < 2", filepath);
+
+            try
+            {
+                var krotFileData = ExtractDataFromKrotTxtFile(lines);
+                var cmpScan = CreateCmpScanFromKrotData(krotFileData);
+
+                return cmpScan;
+            }
+            catch (Exception ex)
+            {
+                throw new FileLoadException("Ошибка при загрузке файла", filepath);
+            }
+        }
+
+        private CmpScan CreateCmpScanFromKrotData(List<double[]> krotFileData)
+        {
+            var cmpScan = new CmpScan {StepTime = krotFileData[1][1] - krotFileData[0][1]};
+            foreach (var piece in krotFileData)
+            {
+                var curAscanNum = Convert.ToInt32(piece[0]);
+                if (curAscanNum == cmpScan.LengthDimensionless)
+                {
+                    var ascanLengthDimensionless = krotFileData.Count(x => x[0] == curAscanNum);
+                    cmpScan.Data.Add(new int[ascanLengthDimensionless]);
+                }
+                var curAscanPoint = Convert.ToInt32(piece[1] / cmpScan.StepTime);
+                cmpScan.Data[curAscanNum][curAscanPoint] = Convert.ToInt32(piece[2]);
+            }
+
+            return cmpScan;
+        }
+
+        private List<double[]> ExtractDataFromKrotTxtFile(string[] lines)
+        {
+            var krotFileData = new List<double[]>();
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var line = lines[i].Substring(0, lines[i].Length - 1);
+                var numAscan = Convert.ToDouble(line.Substring(0, line.IndexOf(";")));
+                var time = Convert.ToDouble(
+                    line.Substring(line.IndexOf(";") + 1, line.LastIndexOf(";") - line.IndexOf(";") - 1));
+                var amplitude = Convert.ToDouble(line.Substring(line.LastIndexOf(";") + 1, line.Length - line.LastIndexOf(";") - 1));
+                krotFileData.Add(new double[] {numAscan, time, amplitude});
+            }
+
+            return krotFileData;
+        }
+
+        public ICmpScan OpenGeo(string filepath)
         {
             throw new System.NotImplementedException();
         }
